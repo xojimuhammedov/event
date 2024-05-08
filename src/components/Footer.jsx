7eeb14947c8c045b1da331e50feae72aa315c1fd
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-05-08 12:15:43 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Logo from "../assets/logo.png";
-import TiktokIcon from "../assets/tiktok.png";
 import InstagramIcon from "../assets/instagram.png";
 const Footer = () => {
   return (
