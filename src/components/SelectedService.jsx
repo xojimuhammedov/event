36cc42079130ab5b8b0a15507c13c65dd88db41b
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-05-10 19:00:32 +0500

    update language

--- src/components/SelectedService.jsx
+++ src/components/SelectedService.jsx
@@ -3,6 +3,7 @@ import Img10 from "../assets/img10.png";
 
 import { BASE_URL } from "../url";
 import axios from "axios";
+import { Link } from "react-router-dom";
 const SelectedService = () => {
   const [res, setRes] = useState([]);
   useEffect(() => {
@@ -21,29 +22,31 @@ const SelectedService = () => {
         <div className="flex justify-center relative z-10">
           <img src={Img10} alt="img" />
         </div>
-        <h3 className="text-center pb-4 font-serif">ÖNE ÇIKAN HİZMETLER</h3>
+        <h3 className="text-center pb-4 text-lg font-serif">ÖNE ÇIKAN HİZMETLER</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 cartContainer flex-wrap">
           {res?.map((item, index) => (
-            <div key={index} className="ritualsContainer">
-              <div className="img-bg">
-                <img
-                  src={`${BASE_URL}${item?.attributes?.Image?.data?.attributes?.url}`}
-                  alt="Hina rituals"
-                  className="selected-image"
-                />
+            <Link to={`/services/${item?.id}`}>
+              <div key={index} className="ritualsContainer">
+                <div className="img-bg">
+                  <img
+                    src={`${BASE_URL}${item?.attributes?.Image?.data?.attributes?.url}`}
+                    alt="Hina rituals"
+                    className="selected-image"
+                  />
+                </div>
+                <div className="bg-[#f6f6f6] w-[400px] absolute z-30 -translate-y-12 translate-x-6 px-4 py-3 textContainer">
+                  <h3 className="text-center font-serif text-[13px] text-[#333] mb-2">
+                    {item?.attributes?.Name}
+                  </h3>
+                  <p className="text-center font-serif text-[15px] text-[#2b333b]">
+                    {item?.attributes?.Title}
+                  </p>
+                  <p className="text-center font-serif text-[12px] text-[#999]">
+                    {item?.attributes?.Organization}
+                  </p>
+                </div>
               </div>
-              <div className="bg-[#f6f6f6] w-[400px] absolute z-30 -translate-y-12 translate-x-6 px-4 py-3 textContainer">
-                <h3 className="text-center font-serif text-[13px] text-[#333] mb-2">
-                  {item?.attributes?.Name}
-                </h3>
-                <p className="text-center font-serif text-[15px] text-[#2b333b]">
-                  {item?.attributes?.Title}
-                </p>
-                <p className="text-center font-serif text-[12px] text-[#999]">
-                  {item?.attributes?.Organization}
-                </p>
-              </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
